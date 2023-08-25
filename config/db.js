8620b6f94770e3ee880233c589a45d72c42b2ecf
Ayan Upadhaya <ayanu881@gmail.com>
2023-08-26 01:53:40 +0600

    connection string added

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDb = async()=>{
     try{
-        const con = await mongoose.connect(`${process.env.MONGO_URI}`);
+        const con = await mongoose.connect("mongodb+srv://ayan:VhaZKP5HvOwpumbY@neonflake.ctgu6hk.mongodb.net/?retryWrites=true&w=majority");
         console.log('Connected to MongoDB ');
     }catch(err){
         console.log(err);

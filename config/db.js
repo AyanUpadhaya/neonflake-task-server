f9219f80ad68edf10342af6cbdc9e70b4eab5e9b
Ayan Upadhaya <ayanu881@gmail.com>
2023-08-26 01:46:42 +0600

    enviroment modified

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const connectDb = async()=>{
     try{
-        const con = await mongoose.connect(process.env.MONGO_URI);
+        const con = await mongoose.connect(`${process.env.MONGO_URI}`);
         console.log('Connected to MongoDB ');
     }catch(err){
         console.log(err);

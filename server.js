aef11c1ac0466b12eea2666437e7bdc24f4e9387
Ayan Upadhaya <ayanu881@gmail.com>
2023-08-26 15:53:14 +0600

    script edited

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({path:'./.env'})
+require('dotenv').config()
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
